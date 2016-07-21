51035e572a6c3c35b5917eda317c8376d23b688b
BobyYadav <yadav.boby@tcs.com>
2016-07-21 18:24:31 +0530

    change in controller

--- sam/admin/src/scripts/controller/shiftController.js
+++ sam/admin/src/scripts/controller/shiftController.js
@@ -1,16 +1,26 @@
 import shiftService from './../services/shiftService.js';
 
+var curr = new Date;
+var weekNumber = moment(curr).week();
+var currentYear = moment(curr).year();
+var currentMonday = moment(curr).startOf('week') + 1;
+var self;
+var ShiftNames;
 export default class ShiftController {
-	constructor($http, $q) {
+
+	constructor($http, $q,NgTableParams,$filter, $timeout) {
+		self=this;
+		this.filter=$filter;
+		this.http=$http;
+		this.q=$q;
+		this.NgTableParams=NgTableParams;
+
 		console.log("person: ");
     this.message = "Hi";
-
-    const curr = new Date;
+		const curr = new Date;
     const weekNo = moment(curr).week();
     const year = moment(curr).year();
-
     const monday = moment(curr).startOf('week') + 1;
-
     console.log("week#: ", weekNo, " year: ", year);
 
     this.message = "Shift Plan for ";
@@ -32,6 +42,7 @@ export default class ShiftController {
 		this.getShifts($http, $q)
 				.then((result) => {
 					this.shifts = result;
+					ShiftNames=this.shifts;
 					console.log("shift master: ", JSON.stringify(result));
 					return this.getEmployeeShifts($http, $q, weekNo, year);
 				})
@@ -47,12 +58,25 @@ export default class ShiftController {
 					});
 
 					console.log("response result: ", JSON.stringify(response));
-					this.weeksShift = response;
+					self.tableParams = new NgTableParams({
+						page: 1,            // show first page
+						count: response.length
+					},
+						{
+						getData: function() {
+							return response;
+						}
+					}
+					);
+					//this.tableParams = new NgTableParams({}, { dataset: response });
+					
 				})
 				.catch((error) => {
 					this.errors.push("no dates were registered for current week");
 				});
 	}
+
+
 
 	getShifts($http, $q){
 		const svc = new shiftService($http, $q);
@@ -83,6 +107,91 @@ export default class ShiftController {
 					return reject(err);
 				});
 		})
+	}
+
+	getNextShift() {
+		weekNumber+=1;
+		var $http=this.http;
+
+		this.dateRange = moment(currentMonday).add(1, 'days').format("YYYY-MM-DD") + " to " +
+			moment(currentMonday).add(7, 'days').format("YYYY-MM-DD");
+
+		var days = [];
+		for (var i = 1; i < 8; i++) {
+			days.push({
+				id: "sday" + (i-1).toString(),
+				date: moment(currentMonday).add(i, 'days').format("YYYY-MM-DD")
+			});
+		}
+
+		this.days = days;
+		this.errors = [];
+		this.a = 5;
+		console.log("days: ", JSON.stringify(days));
+		this.getEmployeeShifts($http, this.q, weekNumber, currentYear)
+			.then((result) => {
+				var response = _.map(result, (i) => {
+					var eo = {};
+					for(var j=0; j<7; j++){
+						var d = i["day"+j.toString()];
+						var dname = _.findWhere(this.shifts, {id: d}).shiftname;
+						eo["sday"+j.toString()] = dname;
+					}
+					return _.extend(i, eo);
+				});
+				console.log("next response",response);
+				console.log("next response1",this.tableParams);
+				self.tableParams = {reload:function(){},settings:function(){return {}}};
+				self.tableParams =  this.NgTableParams({}, { getData: function () {
+					return response;
+				} });
+			})
+			.catch((error) => {
+				this.errors.push("no dates were registered for current week");
+			});
+	}
+
+	getPreviousShift() {
+		weekNumber-=1;
+		var $http=this.http;
+
+
+		this.dateRange = moment(currentMonday).add(1, 'days').format("YYYY-MM-DD") + " to " +
+			moment(currentMonday).add(7, 'days').format("YYYY-MM-DD");
+
+		var days = [];
+		for (var i = 1; i < 8; i++) {
+			days.push({
+				id: "sday" + (i-1).toString(),
+				date: moment(currentMonday).add(i, 'days').format("YYYY-MM-DD")
+			});
+		}
+
+		this.days = days;
+		this.errors = [];
+		this.a = 5;
+		console.log("days: ", JSON.stringify(days));
+		this.getEmployeeShifts($http, this.q, weekNumber, currentYear)
+			.then((result) => {
+				var response = _.map(result, (i) => {
+					var eo = {};
+					for(var j=0; j<7; j++){
+						var d = i["day"+j.toString()];
+						var dname = _.findWhere(this.shifts, {id: d}).shiftname;
+						eo["sday"+j.toString()] = dname;
+					}
+					return _.extend(i, eo);
+				});
+				console.log("next response",response);
+				console.log("next response1",this.tableParams);
+				self.tableParams = {reload:function(){},settings:function(){return {}}};
+				self.tableParams =  this.NgTableParams({}, { getData: function () {
+					return response;
+				} });
+			})
+			.catch((error) => {
+				this.errors.push("no dates were registered for current week");
+			});
 	}
 }
 
