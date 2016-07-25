73e5e9262d23f9bb3820395c25bf61e4f84d5c8e
PrasannaBiswal <prasanna.biswal@tcs.com>
2016-07-25 19:35:15 +0530

    Query Modify and DB Backup

--- sam/services/api/login/queries.js
+++ sam/services/api/login/queries.js
@@ -4,7 +4,7 @@ const squel = require('squel');
 module.exports = {
   userDetails(user) {
     const query = squel.select({tableAliasQuoteCharacter: '', numberedParameters: true})
-      .field("password")
+      .field("passwordkey")
       .field("isadmin")
       .from("master.user")
       .where("username = ?", user);
--- sam/services/app.js
+++ sam/services/app.js
@@ -5,6 +5,43 @@ const server = restify.createServer();
 
 server.use(restify.bodyParser());
 server.use(restify.CORS());
+
+/*server.use(function(){
+  "use strict";
+  var token = req.headers['authorization'];
+  var key   = req.headers['key'];
+
+  if (token || key) {
+    try {
+      jwt.verify(token, require('secret')(), function (err, decoded) {
+        if (err) {
+          res.status(401);
+          return res.json({resTypeCode: "1", status: 401, resTypeMessage: 'Session Expired. Please login again to continue.'});
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+
+    } catch (err) {
+      res.status(500);
+      res.json({
+        "status"        : 500,
+        "resTypeMessage": "something went wrong",
+        "error"         : err
+      });
+    }
+  } else {
+    res.status(401);
+    res.json({
+      "status"        : 401,
+      "resTypeCode"   : "1",
+      "resTypeMessage": "Please login to continue."
+    });
+    return;
+  }
+});*/
+
 server.opts(/.*/, function (req,res,next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
