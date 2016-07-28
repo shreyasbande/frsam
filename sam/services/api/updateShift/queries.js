9a04f390eb59bfd25604602c153e87772a7da6a8
smitamithari <smita.mithari@tcs.com>
2016-07-28 14:06:46 +0530

    insert query

--- sam/services/api/updateShift/queries.js
+++ sam/services/api/updateShift/queries.js
@@ -10,5 +10,28 @@ module.exports = {
        .where("userid=?", params[0])
        .where("dateid=?", squel.select().field(" id").from("master.dates").where("fulldate=?",params[2]));
         return query.toString();
+  },
+  insertShiftData(params) {
+
+    const query = sequel.insert({tableAliasQuoteCharacter: ''})
+      .into("master.dates")
+      .set("sdate", params[0])
+      .set("smonth", params[3])
+      .set("syear", params[2])
+      .set("fulldate", params[1])
+      .set("weekno", params[4])
+      .set("isweekend", true)
+      .set("weekday", "now()", {dontQuote: true});
+    return query.toString();
+  },
+  selectShiftData(params) {
+
+    const  query = squel.select({tableAliasQuoteCharacter: ''})
+      .field("shiftid =?")
+      .from("transaction.shiftdetails")
+      .where("userid=?", params[0])
+      .where("dateid=?", squel.select().field(" id").from("master.dates").where("fulldate=?",params[2]));
+    return query.toString();
   }
-};
+
+  };
